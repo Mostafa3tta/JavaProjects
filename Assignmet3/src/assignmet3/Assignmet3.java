4cb4326993bb59f24a26014efe684c713d042f2f
mostafa 3tta <58670128+Mostafa3tta@users.noreply.github.com>
2021-11-06 23:00:27 +0200

    Add files via upload

---
+++ Assignmet3/src/assignmet3/Assignmet3.java
@@ -0,0 +1,20 @@
+
+package assignmet3;
+
+
+public class Assignmet3 {
+
+    public static void main(String[] args) 
+    {
+        
+  Ball ball = new Football("FootBall");
+  
+  Baseball baseball = (Baseball)ball;
+
+
+Tossable tossable = new Baseball("BaseBall");
+          Object obj = tossable;
+        
+    }
+    
+}
---
+++ Assignmet3/src/assignmet3/Rock.java
@@ -0,0 +1,8 @@
+
+package assignmet3;
+
+public class Rock {
+    
+    public void toss(){}
+    
+}
